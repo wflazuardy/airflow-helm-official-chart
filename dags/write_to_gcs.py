d7dd746412098f3fff87c0c8baae53fea8b1eaaf
Wildan Fajria Lazuardy <wflazuardy@gmail.com>
2023-11-01 14:23:01 +0700

    update gcs example dag bucket dest

--- dags/write_to_gcs.py
+++ dags/write_to_gcs.py
@@ -18,7 +18,7 @@ with DAG(
         task_id='create_example_data',
         run_date='{{ ds }}',
         gcp_conn_id='airflow_gke_gcs_conn_id',
-        gcs_bucket='example-data-bucket'
+        gcs_bucket='ula-data-team'
     )
 
     create_and_write_example_data
